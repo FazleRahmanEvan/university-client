cd31eaf8900e7b9346d05248c7fca20956901cc1
FazleRahmanEvan <evanshakib26@gmail.com>
2024-11-16 21:59:24 +0600

    login features

---
+++ src/pages/faculty/FacultyDashboard.tsx
@@ -0,0 +1,9 @@
+const FacultyDashboard = () => {
+  return (
+    <div>
+      <h2>This Is Faculty</h2>
+    </div>
+  );
+};
+
+export default FacultyDashboard;
---
+++ src/pages/student/StudentDashboard.tsx
@@ -0,0 +1,9 @@
+const StudentDashboard = () => {
+  return (
+    <div>
+      <h3>Student Dashboard</h3>
+    </div>
+  );
+};
+
+export default StudentDashboard;
